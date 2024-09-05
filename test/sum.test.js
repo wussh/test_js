4eb37a51e6fa03f46ca2151b90df3dc9bcaa5d9e
wussh <andrianta.321@gmail.com>
2024-09-05 10:48:06 +0700

    adsad

---
+++ test/sum.test.js
@@ -0,0 +1,13 @@
+const sum = (a,b) => {
+    return a + 1
+    return a + b
+}
+
+
+test('two plus two is four', () => {
+    expect(sum(2,2)).toBe(4);
+  });
+
+test('two plus four is six', () => {
+    expect(sum(2,4)).toBe(6);
+});
